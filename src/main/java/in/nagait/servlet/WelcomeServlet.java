135d92eaf0f43961b567d85a36dc734a5a7fd09e
Nagalakshmi <ipsnagalakshmi@gmail.com>
2022-07-28 20:12:19 +0530

    first commit

---
+++ src/main/java/in/nagait/servlet/WelcomeServlet.java
@@ -0,0 +1,24 @@
+package in.nagait.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/welcome")
+public class WelcomeServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//super.doGet(req, resp);
+		PrintWriter writer=resp.getWriter();
+		writer.write("<h1> Welcome to Web applications </h1>");
+		writer.close();
+	}
+
+}
